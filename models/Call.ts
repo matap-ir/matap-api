82132670eb5637274da4e097948e08f27ed6e553
rezabaiat <Untouchable.rb@gmail.com>
2020-03-22 07:23:52 +0430

    commi

--- models/Call.ts
+++ models/Call.ts
@@ -1,6 +1,8 @@
 interface Call{
     id?: string,
     type: 'video/audio' | 'audio',
+    state: 'initiating' | 'transmitting' | 'ended',
+    initiateTime: number,
     from: {id:string,url?:string},
     to: {id:string,url?:string},
 }
