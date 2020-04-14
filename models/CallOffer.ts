17f5b9d2d64efc3b07547f9b6b396776743cea0e
rezabaiat <Untouchable.rb@gmail.com>
2020-04-14 18:02:24 +0430

    commit

--- models/CallOffer.ts
+++ models/CallOffer.ts
@@ -4,8 +4,9 @@ interface CallOffer{
     type: 'video/audio' | 'audio',
     state: 'initiating' | 'transmitting' | 'ended',
     initiateDate: number,
-    initiator: {id: string,outputUrl: string,inputUrl: string},
-    receiver: {id: string,outputUrl: string,inputUrl: string},
+    iceServers:{url:string,credential:string,username:string,password:string}[]
+    /*initiator: {id: string,outputUrl: string,inputUrl: string},
+    receiver: {id: string,outputUrl: string,inputUrl: string},*/
 }
 
 export default CallOffer;
