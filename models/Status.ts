a51e7e16de24fb554d00f25419fefbda14fee390
rezabaiat <Untouchable.rb@gmail.com>
2020-04-25 13:37:15 +0430

    commit

--- models/Status.ts
+++ models/Status.ts
@@ -1,8 +1,9 @@
-import {User} from '../index';
+import {Specialization, User, UserType} from '../index';
 
 export default interface Status{
     room: {id:string,patient:User,doctor:User} | undefined,
     ready: boolean,
+    type: UserType,
     forDoctor:{
         queueList:User[]
     },
