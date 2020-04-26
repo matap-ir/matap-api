0cd800865cbb28e7088b57009ae803367e6ad6a5
rezabaiat <Untouchable.rb@gmail.com>
2020-04-26 22:56:46 +0430

    commit

--- models/Status.ts
+++ models/Status.ts
@@ -8,6 +8,6 @@ export default interface Status{
     },
     forPatient:{
         queueDoctor:User | undefined,
-        queueDiscountCode: string | undefined
+        queueDiscountId: string | undefined
     }
 }
