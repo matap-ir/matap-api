1478e3175526e182b60a2466b1b768b154c92e36
rezabaiat <Untouchable.rb@gmail.com>
2020-04-26 14:16:05 +0430

    commit

--- index.d.ts
+++ index.d.ts
@@ -8,6 +8,7 @@ import CallOffer from './models/CallOffer';
 import Specialization from './models/specialization/Specialization';
 import HealthCenter from 'models/health_center/HealthCenter';
 import DiscountCoupon from './models/discount/DiscountCoupon';
+import Receipt from 'models/receipt/Receipt';
 
 export {
     CallOffer,
@@ -21,6 +22,7 @@ export {
     Specialization,
     HealthCenter,
     DiscountCoupon,
+    Receipt
 };
 
 export type HTMLTypes = 'text' | 'name' | 'number' | 'password' | 'tel' | 'date' | 'datetime-local' | 'file' | 'image' | 'month' | 'email' | 'url' | 'search';
