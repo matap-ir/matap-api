d05c5b71aceca9b655b3737a5797716799574f63
rezabaiat <Untouchable.rb@gmail.com>
2020-04-27 00:27:31 +0430

    commit

--- models/DoctorStatus.ts
+++ models/DoctorStatus.ts
@@ -3,5 +3,5 @@ import {User} from '../index';
 export default interface DoctorStatus{
     room: {id:string,patient:User,doctor:User} | undefined,
     ready: boolean,
-    queueList:User[]
+    queueList:User[] | string[]
 }
