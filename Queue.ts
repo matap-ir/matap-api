3ec14694141f481946f02adffbfb6ee6b4be934a
rezabaiat <Untouchable.rb@gmail.com>
2020-04-27 00:06:21 +0430

    commit

---
+++ Queue.ts
@@ -0,0 +1,8 @@
+export interface Queue{
+    _id: string,
+    patientId: string,
+    doctorId: string,
+    discountId: string,
+}
+
+export default Queue;
--- models/PatientStatus.ts
+++ models/PatientStatus.ts
@@ -2,5 +2,5 @@ import {User} from '../index';
 
 export default interface PatientStatus{
     room: {id:string,patient:User,doctor:User} | undefined,
-    queue: string | undefined
+    queue: string
 }
