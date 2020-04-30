24cf963029c7a122fa59f665fb8da6c1a295d3c1
rezabaiat <Untouchable.rb@gmail.com>
2020-04-30 21:13:43 +0430

    commit

--- models/transactions/Transaction.ts
+++ models/transactions/Transaction.ts
@@ -1,7 +1,7 @@
 export default interface Transaction{
     _id:string,
     amount:number,
-    type:'CHARGE_BY_GATEWAY' | 'CHARGE_BY_ADMIN' | 'STARTER_CHARGE' | 'VISIT_PAYMENT' | 'PAYROLL',
+    type:'CHARGE_BY_GATEWAY' | 'CHARGE_BY_ADMIN' | 'REDUCE_BY_ADMIN' | 'STARTER_CHARGE' | 'VISIT_PAYMENT' | 'PAYROLL',
     issuer:{
         type: 'admin' | 'user',
         id:string,
