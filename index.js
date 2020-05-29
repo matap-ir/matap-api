03a86892394cae8db2ba06a4cb517e6829e1c04a
rezabaiat <Untouchable.rb@gmail.com>
2020-05-29 20:31:30 +0430

    commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const FileAddresses = require('./constants/FileAddresses').default;
 const {UserType,AdminType,VisitStatus,TransactionType,EventType,HealthCenterType,ChatType,SendStatus,IOStatus,ReceiveStatus} = require('./models/Enums');
-
+const Helper = require('./helpers/Helper').default;
 module.exports= {
     UserType,
     AdminType,
@@ -13,4 +13,5 @@ module.exports= {
     IOStatus,
     ReceiveStatus,
     FileAddresses,
+    Helper
 };
