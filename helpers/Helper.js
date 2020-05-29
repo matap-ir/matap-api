d976e0490ee3bb06451039b7f59a8208c9cf4e21
rezabaiat <Untouchable.rb@gmail.com>
2020-05-29 19:09:04 +0430

    commit

--- helpers/Helper.js
+++ helpers/Helper.js
@@ -5,6 +5,25 @@ const v4_1 = tslib_1.__importDefault(require("uuid/v4"));
 const generateUUID = () => {
     return v4_1.default();
 };
+const dayNumberToString = (day) => {
+    switch (day) {
+        case '0':
+            return 'یکشنبه';
+        case '1':
+            return 'دوشنبه';
+        case '2':
+            return 'سه شنبه';
+        case '3':
+            return 'چهارشنبه';
+        case '4':
+            return 'پنجشنبه';
+        case '5':
+            return 'جمعه';
+        case '6':
+            return 'شنبه';
+    }
+};
 exports.default = {
     generateUUID,
+    dayNumberToString
 };
--- helpers/Helper.ts
+++ helpers/Helper.ts
@@ -4,6 +4,26 @@ const generateUUID = () => {
     return uuidv4();
 };
 
+const dayNumberToString = (day) => {
+    switch (day) {
+        case '0':
+            return 'یکشنبه';
+        case '1':
+            return 'دوشنبه';
+        case '2':
+            return 'سه شنبه';
+        case '3':
+            return 'چهارشنبه';
+        case '4':
+            return 'پنجشنبه';
+        case '5':
+            return 'جمعه';
+        case '6':
+            return 'شنبه';
+    }
+};
+
 export default {
     generateUUID,
+    dayNumberToString
 };
