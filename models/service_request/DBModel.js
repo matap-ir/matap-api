999b5df718f54589b5fb4a1e8984618b95e49e6f
rezabaiat <Untouchable.rb@gmail.com>
2020-06-29 09:42:52 +0430

    update

--- models/service_request/DBModel.js
+++ models/service_request/DBModel.js
@@ -4,7 +4,7 @@ const mongoose_1 = require("mongoose");
 const dbModel = {
     age: { type: Number, required: true },
     mobile: { type: String, required: true },
-    service: { type: mongoose_1.ObjectId, ref: 'service_requests', required: true },
+    service: { type: mongoose_1.ObjectId, ref: 'medical_services', required: true },
     gender: { type: String, required: true },
     request_date: { type: Number, required: true },
     details: { type: String, required: true }
