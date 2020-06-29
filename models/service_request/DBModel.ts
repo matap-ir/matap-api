2d5106a1f1805b62770cbbe94614a6ca56578183
rezabaiat <Untouchable.rb@gmail.com>
2020-06-29 09:42:49 +0430

    update

--- models/service_request/DBModel.ts
+++ models/service_request/DBModel.ts
@@ -2,7 +2,7 @@ import { ObjectId } from 'mongoose';
 const dbModel = {
     age:{type:Number,required:true},
     mobile:{type:String,required:true},
-    service:{type:ObjectId,ref:'service_requests',required:true},
+    service:{type:ObjectId,ref:'medical_services',required:true},
     gender:{type:String,required: true},
     request_date:{type:Number,required:true},
     details:{type:String,required:true}
