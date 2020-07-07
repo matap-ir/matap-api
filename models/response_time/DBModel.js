00134f8881d889385f0e9d37a56ba5f712a23cd2
rezabaiat <Untouchable.rb@gmail.com>
2020-07-07 22:53:15 +0430

    update

--- models/response_time/DBModel.js
+++ models/response_time/DBModel.js
@@ -2,8 +2,8 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose_1 = require("mongoose");
 const dbModel = {
-    from: { type: { hour: String, minute: String }, required: true },
-    to: { type: { hour: String, minute: String }, required: true },
+    from: { type: { hour: String, minute: String }, required: false },
+    to: { type: { hour: String, minute: String }, required: false },
     healthCenter: { type: mongoose_1.ObjectId, ref: 'healthcenters', required: false }
 };
 exports.default = dbModel;
