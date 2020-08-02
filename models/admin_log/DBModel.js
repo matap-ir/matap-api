1e8d4eb46c19bb426d6b758a90c0cbd3cc0ac865
rezabaiat <Untouchable.rb@gmail.com>
2020-08-02 13:15:08 +0430

    update

--- models/admin_log/DBModel.js
+++ models/admin_log/DBModel.js
@@ -5,7 +5,7 @@ const dbModel = {
     url: { type: String, required: true },
     path: { type: String, required: true },
     headers: { type: [String], required: false, default: [] },
-    user: { type: mongoose_1.ObjectId, required: true, ref: 'users' },
+    user: { type: mongoose_1.ObjectId, required: true, ref: 'admins' },
     description: { type: String, required: false },
     date: { type: Number, required: true },
     body: { type: Object, required: false },
