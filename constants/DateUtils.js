214e8f8ca98c590d71e6c0b03cb4d3b9858b1621
rezabaiat <Untouchable.rb@gmail.com>
2020-12-02 13:27:44 +0330

    update

---
+++ constants/DateUtils.js
@@ -0,0 +1,100 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.hourMinuteFormat = exports.fullDateFormat = void 0;
+const tslib_1 = require("tslib");
+const moment_1 = tslib_1.__importDefault(require("moment"));
+exports.fullDateFormat = 'YYYY/MM/DD HH:mm';
+exports.hourMinuteFormat = 'HH:mm';
+const formats = [
+    'HH:mm',
+    'YYYY/MM/DD HH:mm',
+    'YYYY/MM/DD',
+];
+const getDayName = (date) => {
+    if (typeof date === 'number') {
+        date = moment_1.default(date).format(exports.fullDateFormat);
+    }
+    return moment_1.default(date, exports.fullDateFormat).format('dddd').toLowerCase();
+};
+const findFormat = (date) => {
+    return formats.find((f) => {
+        try {
+            const m = moment_1.default(date, f);
+            if (m.format(f) === date) {
+                return f;
+            }
+        }
+        catch (e) {
+            return undefined;
+        }
+    });
+};
+const convertToMoment = (date) => {
+    if (typeof date === 'number') {
+        return moment_1.default(date);
+    }
+    else if (typeof date === 'object') {
+        return date;
+    }
+    else if (date) {
+        return moment_1.default(date, findFormat(date));
+    }
+    else {
+        return undefined;
+    }
+};
+const toYYYYMMDD = (date) => {
+    return convertToMoment(date).format('YYYY/MM/DD');
+};
+const isValidOption = (option, reserved, workTimes, gapMinutes) => {
+    const options = findOptions(option.from, option.to, reserved, workTimes, gapMinutes);
+    return Boolean(options.find(o => o.from === option.from && o.to === option.to));
+};
+const toYYYYMMDDHHmm = (date) => {
+    return convertToMoment(date).format('YYYY/MM/DD HH:mm');
+};
+const isInMiddle = (from, middle, to, offset = 0) => {
+    const fromTime = convertToMoment(toYYYYMMDDHHmm(from)).toDate().getTime() - offset;
+    const middleTime = convertToMoment(toYYYYMMDDHHmm(middle)).toDate().getTime();
+    const toTime = convertToMoment(toYYYYMMDDHHmm(to)).toDate().getTime() + offset;
+    return fromTime < middleTime && toTime > middleTime;
+};
+const findOptions = (fromTime, toTime, reserved, workTimes, gapMinutes) => {
+    const nowDate = moment_1.default(fromTime);
+    const options = [];
+    while (nowDate.toDate().getTime() < toTime) {
+        const dayString = getDayName(nowDate);
+        workTimes[dayString].forEach((workTime) => {
+            if (workTime.exceptions.length > 0 && workTime.exceptions.includes(toYYYYMMDD(nowDate))) {
+                return;
+            }
+            const fromMoment = moment_1.default(nowDate.toDate().getTime()).hour(convertToMoment(workTime.from).hour()).minutes(convertToMoment(workTime.from).minutes());
+            const toMoment = moment_1.default(nowDate.toDate().getTime()).hour(convertToMoment(workTime.to).hour()).minutes(convertToMoment(workTime.to).minutes());
+            let timeIndex = fromMoment.toDate().getTime();
+            const gapMilli = gapMinutes * 60 * 1000;
+            while (timeIndex + gapMilli <= toMoment.toDate().getTime()) {
+                const res = reserved.find((r) => {
+                    if (isInMiddle(r.from, timeIndex, r.to) || isInMiddle(r.from, timeIndex + gapMilli, r.to)) {
+                        return true;
+                    }
+                });
+                if (!res) {
+                    options.push({ from: timeIndex, to: timeIndex + gapMilli });
+                }
+                timeIndex += gapMilli;
+            }
+        });
+        nowDate.add(1, 'day');
+    }
+    return options;
+};
+exports.default = {
+    getDayName,
+    toYYYYMMDD,
+    toYYYYMMDDHHmm,
+    convertToMoment,
+    findFormat,
+    isInMiddle,
+    findOptions,
+    isValidOption
+};
---
+++ constants/DateUtils.ts
@@ -0,0 +1,103 @@
+import moment from 'moment';
+import {WorkTime} from '../index';
+
+export const fullDateFormat = 'YYYY/MM/DD HH:mm';
+export const hourMinuteFormat = 'HH:mm';
+const formats = [
+    'HH:mm',
+    'YYYY/MM/DD HH:mm',
+    'YYYY/MM/DD',
+]
+
+const getDayName = (date: number | string | moment.Moment): string=>{
+    if(typeof date === 'number'){
+        date = moment(date).format(fullDateFormat);
+    }
+    return moment(date,fullDateFormat).format('dddd').toLowerCase() as any;
+};
+
+const findFormat = (date: string): string | undefined=>{
+    return formats.find((f: string)=>{
+        try{
+            const m = moment(date,f);
+            if(m.format(f) === date){
+                return f;
+            }
+        }catch (e){
+            return undefined;
+        }
+    });
+}
+
+const convertToMoment = (date: number | string | moment.Moment): moment.Moment=>{
+    if(typeof date === 'number') {
+        return moment(date);
+    }else if(typeof date === 'object'){
+        return date;
+    }else if(date){
+        return moment(date,findFormat(date));
+    }else{
+        return undefined;
+    }
+};
+
+const toYYYYMMDD = (date: string | number | moment.Moment): string=>{
+    return convertToMoment(date).format('YYYY/MM/DD');
+};
+
+const isValidOption = (option: {from: number,to: number},reserved: {from: number,to: number}[],workTimes: any,gapMinutes: number): boolean=>{
+    const options = findOptions(option.from,option.to,reserved,workTimes,gapMinutes);
+    return Boolean(options.find(o => o.from === option.from && o.to === option.to));
+};
+
+const toYYYYMMDDHHmm = (date: string | number | moment.Moment): string=>{
+    return convertToMoment(date).format('YYYY/MM/DD HH:mm');
+};
+
+const isInMiddle = (from: string | moment.Moment | number,middle: string | moment.Moment | number,to: string | moment.Moment | number,offset = 0): boolean=>{
+    const fromTime = convertToMoment(toYYYYMMDDHHmm(from)).toDate().getTime() - offset;
+    const middleTime = convertToMoment(toYYYYMMDDHHmm(middle)).toDate().getTime();
+    const toTime = convertToMoment(toYYYYMMDDHHmm(to)).toDate().getTime() + offset;
+    return fromTime < middleTime && toTime > middleTime;
+};
+
+const findOptions = (fromTime: number, toTime: number, reserved: {from: number, to: number}[], workTimes: any, gapMinutes: number): {from: number, to: number}[] => {
+    const nowDate = moment(fromTime);
+    const options: {from: number, to: number}[] = [];
+    while (nowDate.toDate().getTime() < toTime) {
+        const dayString = getDayName(nowDate);
+        workTimes[dayString].forEach((workTime) => {
+            if (workTime.exceptions.length > 0 && workTime.exceptions.includes(toYYYYMMDD(nowDate))) {
+                return;
+            }
+            const fromMoment = moment(nowDate.toDate().getTime()).hour(convertToMoment(workTime.from).hour()).minutes(convertToMoment(workTime.from).minutes());
+            const toMoment = moment(nowDate.toDate().getTime()).hour(convertToMoment(workTime.to).hour()).minutes(convertToMoment(workTime.to).minutes());
+            let timeIndex = fromMoment.toDate().getTime();
+            const gapMilli = gapMinutes * 60 * 1000;
+            while (timeIndex + gapMilli <= toMoment.toDate().getTime()) {
+                const res = reserved.find((r) => {
+                    if (isInMiddle(r.from, timeIndex, r.to) || isInMiddle(r.from, timeIndex + gapMilli, r.to)) {
+                        return true;
+                    }
+                });
+                if (!res) {
+                    options.push({ from: timeIndex, to: timeIndex + gapMilli });
+                }
+                timeIndex += gapMilli;
+            }
+        });
+        nowDate.add(1, 'day');
+    }
+    return options;
+};
+
+export default {
+    getDayName,
+    toYYYYMMDD,
+    toYYYYMMDDHHmm,
+    convertToMoment,
+    findFormat,
+    isInMiddle,
+    findOptions,
+    isValidOption
+}
