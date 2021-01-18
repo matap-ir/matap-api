cc2be71bbe7f2d7ac5b9153bc7fd4c6d45183d00
rezabaiat <Untouchable.rb@gmail.com>
2021-01-18 20:32:41 +0330

    update

--- models/users/DBModel.ts
+++ models/users/DBModel.ts
@@ -1,11 +1,11 @@
 import {Mongoose, ObjectId,SchemaOptions,SchemaDefinition} from 'mongoose';
 import {WorkTime} from '../../index';
 const dbModel: SchemaDefinition = {
-    mobile: {type: String,index:true,unique:true,text: true, required: true},
+    mobile: {type: String,index:true,unique:true, required: true},
     type:{type: String,require:true},
-    name:{type:String,required:false,default:'',text: true},
+    name:{type:String,required:false,default:''},
     imageUrl:{type:String},
-    code:{type:Number,index:true,unique:true,sparse:true,text: true},
+    code:{type:Number,index:true,unique:true,sparse:true},
     specialization:{type:ObjectId,ref:'specializations'},
     price:{type:Number},
     currency:{type:Number,default:0,required:true},
