fbb2779fcb793cd8d6c34db496f016cc5f165727
rezabaiat <Untouchable.rb@gmail.com>
2021-01-28 17:23:39 +0330

    update

--- dist/models/createValidator.d.ts
+++
@@ -1,2 +0,0 @@
-import { ValidatorModel } from '../index';
-export default function createValidator(fields: {}, options?: any): ValidatorModel;
--- dist/models/visit/DBModel.d.ts
+++
@@ -1,3 +0,0 @@
-import { SchemaDefinition } from 'mongoose';
-declare const _default: SchemaDefinition;
-export default _default;
