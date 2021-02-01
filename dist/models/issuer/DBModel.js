f83b6e5f12e3aa1f1ae3f5b667265dab01ce7ada
rezabaiat <Untouchable.rb@gmail.com>
2021-02-01 16:39:32 +0330

    update

--- dist/models/issuer/DBModel.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const __1 = require("../..");
-const dbModel = {
-    type: { type: String, required: true },
-    user: { type: __1.ObjectId, ref: 'users', required: false },
-    admin: { type: __1.ObjectId, ref: 'admins', required: false }
-};
-exports.default = dbModel;
--- dist/models/response_time/DBModel.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const index_1 = require("../../index");
-const dbModel = {
-    from: { type: { hour: String, minute: String }, required: true },
-    to: { type: { hour: String, minute: String }, required: true },
-    healthCenter: { type: index_1.ObjectId, ref: 'healthcenters', required: false }
-};
-exports.default = dbModel;
