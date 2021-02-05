94d023c02a96ebebc8b7f76af9ef55828865cc90
rezabaiat <Untouchable.rb@gmail.com>
2021-02-05 04:36:40 +0330

    update

---
+++ mongoose-types.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.ObjectId = void 0;
+const tslib_1 = require("tslib");
+const mongoose = tslib_1.__importStar(require("mongoose"));
+const ObjectId = mongoose.Types.ObjectId;
+exports.ObjectId = ObjectId;
