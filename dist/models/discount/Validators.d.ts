3f61ce4b4c67e96db0ac680a6aa95f869946d0d5
rezabaiat <Untouchable.rb@gmail.com>
2021-02-05 04:32:02 +0330

    update

--- dist/models/discount/Validators.d.ts
+++
@@ -1,4 +0,0 @@
-import { GeneratedValidator } from '../Validator';
-import DiscountCoupon from './DiscountCoupon';
-declare const validator: GeneratedValidator<DiscountCoupon>;
-export default validator;
--- mongoose-types.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.ObjectId = void 0;
-var mongoose = require("mongoose");
-var ObjectId = mongoose.Types.ObjectId;
-exports.ObjectId = ObjectId;
-//# sourceMappingURL=mongoose-types.js.map
