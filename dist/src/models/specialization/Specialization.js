23d150804d4fb3780feea62fdea86a8ad099b9b3
Reza <Untouchable.rb@gmail.com>
2022-01-17 16:59:06 +0330

    update

--- dist/src/models/specialization/Specialization.js
+++ dist/src/models/specialization/Specialization.js
@@ -5,10 +5,6 @@ const mongoose_types_1 = require("../../mongoose-types");
 let Specialization = class Specialization {
 };
 tslib_1.__decorate([
-    mongoose_types_1.Prop({ type: mongoose_types_1.ObjectId }),
-    tslib_1.__metadata("design:type", String)
-], Specialization.prototype, "_id", void 0);
-tslib_1.__decorate([
     mongoose_types_1.Prop(),
     tslib_1.__metadata("design:type", String)
 ], Specialization.prototype, "name", void 0);
