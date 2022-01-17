7f4b41ea22db26da2480403ffc95124b20d65c3c
Reza <Untouchable.rb@gmail.com>
2022-01-17 15:48:49 +0330

    update

--- src/models/createValidator.js
+++ src/models/createValidator.js
@@ -1,17 +1,28 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const tslib_1 = require("tslib");
-const Joi_1 = tslib_1.__importDefault(require("./Joi"));
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
+exports.__esModule = true;
+var Joi_1 = require("./Joi");
 function createValidator(fields, options) {
-    const validator = Joi_1.default.object().keys(fields);
-    const newFields = Object.assign({}, fields);
-    Object.keys(newFields).forEach((key) => {
+    var validator = Joi_1["default"].object().keys(fields);
+    var newFields = __assign({}, fields);
+    Object.keys(newFields).forEach(function (key) {
         newFields[key] = { validator: newFields[key], metadata: {} };
     });
     validator.options = options;
     return {
         fields: newFields,
-        validator,
+        validator: validator,
     };
 }
-exports.default = createValidator;
+exports["default"] = createValidator;
+//# sourceMappingURL=createValidator.js.map
