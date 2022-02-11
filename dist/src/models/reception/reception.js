50ac8a7f382f1fb2c9b7ba105732869b1f6635e8
rezabaiat_xcoins <Untouchable.rb@gmail.com>
2022-02-11 21:56:30 +0330

    update

---
+++ dist/src/models/reception/reception.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const tslib_1 = require("tslib");
+const mongoose_types_1 = require("../../mongoose-types");
+let Reception = class Reception {
+};
+Reception = tslib_1.__decorate([
+    mongoose_types_1.Schema()
+], Reception);
+exports.default = Reception;
